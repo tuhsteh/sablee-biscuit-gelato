3c11520160d5ba5b5879c59ba147a65aa3e88309
tommy tom <t.stear@gmail.com>
2025-09-11 21:02:44 -0500

    adding two more values to .env example; maybe i can prettier a db-entrypoint.js

--- config/db-entrypoint.js
+++ config/db-entrypoint.js
@@ -7,15 +7,14 @@
 
 db.getSiblingDB(process.env.MONGODB_INITDB_DATABASE).auth(
   process.env.MONGODB_INITDB_ROOT_USERNAME,
-  process.env.MONGODB_INITDB_ROOT_PASSWORD
+  process.env.MONGODB_INITDB_ROOT_PASSWORD,
 );
 
 // Create the first InvitationCode, with
 // known values.
 db.getDatabase(process.env.MONGODB_DB);
 
-db.getCollection(invitations)
-  .insertOne({
-    invite_email: process.env.start_admin_email,
-    invite_code: process.env.start_invite_code
-  });
+db.getCollection(invitations).insertOne({
+  invite_email: process.env.start_admin_email,
+  invite_code: process.env.start_invite_code,
+});
