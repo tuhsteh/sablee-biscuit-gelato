9c517d3cf4cadc477df29c2dfeed3c60faa931a7
tommy tom <t.stear@gmail.com>
2025-09-11 20:58:28 -0500

    That was never gonna work; the variable names were wrong too; this db-entrypoint.js needs more testing; add an empty database with it, using a different initial db name

--- config/db-entrypoint.js
+++ config/db-entrypoint.js
@@ -5,18 +5,17 @@
 // i.e. on first start.  that_s_pretty_neat.tiff
 // https://gist.github.com/x-yuri/c229b3f9e8282ea12cecbbe923cc61c1
 
-db.getSiblingDB(${MONGO_INITDB_DATABASE}).auth(
-  process.env.MONGO_INITDB_ROOT_USERNAME,
-  process.env.MONGO_INITDB_ROOT_PASSWORD
+db.getSiblingDB(process.env.MONGODB_INITDB_DATABASE).auth(
+  process.env.MONGODB_INITDB_ROOT_USERNAME,
+  process.env.MONGODB_INITDB_ROOT_PASSWORD
 );
 
-db.createUser({
-  user: process.env.MONGO_USERNAME,
-  pwd: process.env.MONGO_PASSWORD,
-  roles: ['readWrite'],
-});
+// Create the first InvitationCode, with
+// known values.
+db.getDatabase(process.env.MONGODB_DB);
 
-// use process.env.MONGO_DB);  
-// this command is useless here;
-// the server will make the DB only when data
-// is saved in a Collection in the DB.
+db.getCollection(invitations)
+  .insertOne({
+    invite_email: process.env.start_admin_email,
+    invite_code: process.env.start_invite_code
+  });
